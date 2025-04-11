c1ee98de33a60d08e87ef68b0d517e9ac65c85b9
defendimattia <defendi.mattia92@gmail.com>
2025-04-11 15:56:20 +0200

    add> actors list and logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { useEffect } from "react"
 import Card from "./assets/Card"
 import './App.css'
 
-const actressndpoint = "https://www.freetestapi.com/api/v1/actresses"
+const actressEndpoint = "https://www.freetestapi.com/api/v1/actresses"
+const actorsEndpoint = "https://www.freetestapi.com/api/v1/actors"
 
 
 function App() {
 
   const [actressList, setActressList] = useState([])
+  const [actorsList, setActorsList] = useState([])
 
   useEffect(() => {
-    axios.get(actressndpoint)
+    axios.get(actressEndpoint)
       .then(res => setActressList(res.data))
+    axios.get(actorsEndpoint)
+      .then(res => setActorsList(res.data))
   }, [])
 
   return (
@@ -21,6 +25,10 @@ function App() {
       <h1>Lista attrici</h1>
       <div className="container">
           {actressList.map(actress => <Card key={actress.id} element={actress}/>)}
+      </div>
+      <h1>Lista attori</h1>
+      <div className="container">
+          {actorsList.map(actress => <Card key={actress.id} element={actress}/>)}
       </div>
 
     </>
