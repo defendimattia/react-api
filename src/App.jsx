2d7a3303619b299ed69d438c95e84c7f9668a695
defendimattia <defendi.mattia92@gmail.com>
2025-04-11 15:01:42 +0200

    add: api call and console.log of actress (milestone1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+
+const endpoint = "https://www.freetestapi.com/api/v1/actresses"
+
 
 function App() {
+
+  const [actressList, setActressList] = useState([])
+
+  useEffect(() => {
+    axios.get(endpoint)
+      .then(res => setActressList(res.data))
+  }, [])
+
   return (
     <>
-    <h1>Lista attrici</h1>
-    <ul>
-      
-    </ul>
+      <h1>Lista attrici</h1>
+      <ul>
+    {actressList.map(actress => console.log(actress))}
+      </ul>
     </>
   )
 }
