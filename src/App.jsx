69b21ac0baa92038ffcef7be057fe69324f8a3ff
defendimattia <defendi.mattia92@gmail.com>
2025-04-11 16:17:00 +0200

    add: actors and actress in the same array list

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,25 @@ const actorsEndpoint = "https://www.freetestapi.com/api/v1/actors"
 
 function App() {
 
-  const [actressList, setActressList] = useState([])
   const [actorsList, setActorsList] = useState([])
 
   useEffect(() => {
-    axios.get(actressEndpoint)
-      .then(res => setActressList(res.data))
     axios.get(actorsEndpoint)
       .then(res => setActorsList(res.data))
+    axios.get(actressEndpoint)
+      .then(res => setActorsList(prev => [...prev, ...res.data]))
   }, [])
 
-  return (
-    <>
-      <h1>Lista attrici</h1>
-      <div className="container">
-          {actressList.map(actress => <Card key={actress.id} element={actress}/>)}
-      </div>
-      <h1>Lista attori</h1>
-      <div className="container">
-          {actorsList.map(actress => <Card key={actress.id} element={actress}/>)}
-      </div>
 
-    </>
-  )
+return (
+  <>
+    <h1>Lista attori e attrici</h1>
+    <div className="container">
+      {actorsList.map(actor => <Card key={actor.name.replace(/\s+/g, "-").toLowerCase()} element={actor} />)}
+    </div>
+
+  </>
+)
 }
 
 export default App
